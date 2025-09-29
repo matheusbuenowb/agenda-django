3b93c2c7b5de54a641fa213a3c19fa3a596aee5e
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-09-29 09:38:44 -0300

    Opções do model customizados no admin do django

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-   ...
+   list_display = 'first_name', 'last_name', 'email', 'phone', 'created_at' #mostra essas colunas na lista de contatos
+   ordering = 'id', #ordena por id
+   search_fields = 'id', 'first_name', 'last_name', 'email', 'phone' #busca por esses campos
+   list_max_show_all = 50 #limita a quantidade de contatos mostrados na lista
+   list_editable = 'email','phone' #permite editar esses campos diretamente na lista de contatos
