32e519c98c0547124d3b9dfc25d78c87fa56284f
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-09-29 09:27:21 -0300

    Registrando um model no admin do django

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 # id (criado automaticamente pelo Django como chave primária)
 
 class Contact(models.Model): # herdando de models.Model
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50) #blank=True permite que o campo seja opcional
     last_name = models.CharField(max_length=50, blank=True) #blank=True permite que o campo seja opcional
     email = models.EmailField(max_length=254, blank=True) #blank=True permite que o campo seja opcional
     phone = models.CharField(max_length=50)
@@ -14,4 +14,5 @@ class Contact(models.Model): # herdando de models.Model
     description = models.TextField(blank=True) # campo de texto maior, opcional 
 
     def __str__(self):
-        return f"Contact from {self.name} <{self.email}>"
+        return f"{self.first_name} {self.last_name}" # o que eu vou ver 
+    #quando chamar o objeto Contact no shell do Django
