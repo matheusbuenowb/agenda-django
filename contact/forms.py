fe5bc85e04c0a957be80d42faef3bfd038a2087e
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-10-06 10:22:55 -0300

    Inseridos widgets via django html e python (init)

--- contact/forms.py
+++ contact/forms.py
@@ -3,12 +3,42 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+
+    first_name = forms.CharField( #sobrescrevendo o campo first_name do modelo Contact  
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'classe-a classe-b',
+                'placeholder': 'Digite o primeiro nome',
+            }   
+        ),
+        label = 'Primeiro Nome',
+        help_text = 'Digite o primeiro nome do contato',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['first_name'].widget.attrs.update({ #attrs é um dicionário
+            'class': 'classe-a classe-b',
+            'placeholder': 'Digite o primeiro nome',
+        })
+    
+    
     class Meta:
         model = models.Contact
-        fields = [
+        fields = (
             'first_name',
             'last_name',
-            'phone',]
+            'phone',
+        )
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs = {
+                    'class': 'classe-a classe-b',
+                    'placeholder': 'Digite o primeiro nome',
+                }
+            )
+        }
         
     def clean(self):
         cleaned_data = self.cleaned_data
--- contact/models.py
+++ contact/models.py
@@ -18,7 +18,7 @@ class Contact(models.Model): # herdando de models.Model
     first_name = models.CharField(max_length=50) #blank=True permite que o campo seja opcional
     last_name = models.CharField(max_length=50, blank=True) #blank=True permite que o campo seja opcional
     email = models.EmailField(max_length=254, blank=True) #blank=True permite que o campo seja opcional
-    phone = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, verbose_name='Telefone') #verbose_name altera o nome do campo na interface de administração
     created_at = models.DateTimeField(default=timezone.now) # data e hora de criação do contato
     description = models.TextField(blank=True) # campo de texto maior, opcional 
     show = models.BooleanField(default=True) # campo booleano para mostrar ou não o contato
