8432e8abda5683a34f5dd6c4bc1e3303b5db1434
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-10-08 16:32:00 -0300

    Adicionado messages djang

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
+from django.contrib import messages
 
 def register(request):
 
     form = RegisterForm()
+
+    #messages.info(request, 'Um texto qualquer')
+    #messages.success(request, 'Um texto qualquer')
+    #messages.warning(request, 'Um texto qualquer')
+    #messages.error(request, 'Um texto qualquer')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
+
 
     return render(
         request,
